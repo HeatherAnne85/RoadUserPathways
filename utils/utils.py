36f6a0c94c22680a23305cc915a45f32f7f7d446
HeatherAnne85 <116355810+HeatherAnne85@users.noreply.github.com>
2022-10-21 15:23:01 +0200

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 """
-file: utils.py
-description: working with trajectories
+file: traj_utils.py
+description: trajectory utils
 author: Heather Kaths
 date: 21-10-2022
 """
@@ -11,9 +11,9 @@ import shapely.geometry as SG
 
 def checkIn(traj,poly):
     #return true if a trajectory starts or ends within a shapely polygone
-    P0=obj.positions[0]
-    PL=obj.positions[-1]
-    if delete.contains(SG.Point(P0.x,P0.y)) == False and delete.contains(SG.Point(PL.x,PL.y)) == False:
+    P0=traj.positions[0]
+    PL=traj.positions[-1]
+    if poly.contains(SG.Point(P0.x,P0.y)) == False and poly.contains(SG.Point(PL.x,PL.y)) == False:
         return False
     else:
         return True
