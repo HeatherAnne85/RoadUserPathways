37147bbe79d54f7ec295b183b15c063d3b8d4853
HeatherAnne85 <116355810+HeatherAnne85@users.noreply.github.com>
2022-11-16 15:37:14 +0100

    Update clustering_main.py
    loop through files

--- clustering_main.py
+++ clustering_main.py
@@ -12,23 +12,47 @@ from utils import clustering_utils as cu
 #Parameters
 num_points=20                        #number of x-y coordinates in reduced trajectory
 traj_min_length = 100       #cut off length for too short trajectories in timesteps
-traj_min_number = 10
-num_SQL = 25
+traj_min_number = 15
+num_SQL = 5
+trim = True
+delete = True
+road_user_types = [1]
+define_use = 'use'
+cluster_omit = 2            #minimum size of cluster to be included
 
+'''
+RoadUserTypes = {"unknown": 0, 
+                "undefined": 0,
+                "car": 1,
+                "pedestrian": 2,
+                "motorcycle": 3,
+                "bicycle": 4,
+                "truck_bus": 5}
+'''
 
 #File directory
-Home='C:/Users/heath/Desktop/trajectory_data/TUM/Karl/'
+Homes={'C:/Users/heath/Desktop/trajectory_data/inD-dataset-v1.0/IntersectionA/': ['N','E','S'],
+       'C:/Users/heath/Desktop/trajectory_data/inD-dataset-v1.0/IntersectionB/': ['N','E','S','W'],
+       'C:/Users/heath/Desktop/trajectory_data/inD-dataset-v1.0/IntersectionC/': ['N','E','S','W'],
+       'C:/Users/heath/Desktop/trajectory_data/inD-dataset-v1.0/IntersectionD/': ['N','E','S']}
+    
+'''
 
+'''
 
-#Load geometric information
-intersection = cu.Intersection()
-intersection.define_geometry(Home+'GeometryTest')
-#intersection.load_geometry(Home+'GeometryTest')
-
-#extract, cluster and plot trajectories
-observations = cu.Clusters(Home, intersection, traj_min_length, num_points, num_SQL)
-for approach in ['all', 'N', 'E', 'S', 'W']:
-    observations.cluster_trajectories(approach, plot = True, table = True)
+for Home, approaches in Homes.items():
+    #Load geometric information
+    intersection = cu.Intersection()
+    if define_use == 'use':
+        intersection.load_geometry(Home+'Geometry')
+    else:
+        intersection.define_geometry(Home+'Geometry', approaches)
+    
+    #extract, cluster and plot trajectories
+    for road_user_type in road_user_types:
+        observations = cu.Clusters(Home, intersection, traj_min_length, num_points, trim, delete, num_SQL, road_user_type, cluster_omit, obs_list=[])
+        for approach in ['all'] + approaches:
+            observations.cluster_trajectories(approach, plot = True, table = True)
               
 
     
