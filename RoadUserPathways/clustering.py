7fcda9ae262b2ad480fcfe3ed0109928a5f34c82
HeatherAnne85 <116355810+HeatherAnne85@users.noreply.github.com>
2022-11-22 21:35:45 +0100

    Update clustering.py

--- RoadUserPathways/clustering.py
+++ RoadUserPathways/clustering.py
@@ -56,28 +56,22 @@ class Observation(object):
             self.in_poly = True
             
     def get_length(self):
+        '''get total distance travelled by the road user'''
         self.length = self.trajectory.getTotalDistance()
 
     
     def get_f_star(self):
+        '''locate position coordinates at given distance cut-off points'''
         self.trajectory.computeCumulativeDistances()
         Length=np.floor(self.trajectory.getCumulativeDistance(len(self.trajectory)-1))
-        reduced=[]
-        for X in range(self.num_points):
-            minimum=1000
-            goal=X*Length/self.num_points
-            for pos in range(len(self.trajectory)-1):
-                dist=self.trajectory.getCumulativeDistance(pos)
-                app=np.abs(goal-dist)
-                if app<minimum:
-                    minimum=app
-                else:
-                    reduced.extend([self.trajectory[pos-1].x, self.trajectory[pos-1].y])
-                    break
-        if len(reduced) == 2*self.num_points:
-            self.f_star = reduced
-        else:
-            self.f_star = []
+        goal = np.array([X*Length/self.num_points for X in range(self.num_points)])
+        distances = np.array([self.trajectory.getCumulativeDistance(pos) for pos in range(len(self.trajectory)-1)])
+        indexes = abs(goal[:, None] - distances).argmin(axis=1)
+        out = list(enumerate(indexes))
+        reduced = []
+        for pair in out:
+            reduced.extend([self.trajectory[int(pair[1])].x, self.trajectory[int(pair[1])].y])
+        self.f_star = reduced
     
 
 
@@ -103,6 +97,7 @@ class Clusters(object):
         
     
     def load_observations(self):   
+        '''function to load observations from an SQLite database and append to a list of observations'''
         c = 0 
         for traj_sql in os.listdir(self.filedirectory+'SQLite/'):
             if c < self.num_SQL:
@@ -126,34 +121,36 @@ class Clusters(object):
             c+=1
                             
                             
-   
+    def find_optimal_preference(self, ss, clusters):
+        '''function to find the preference value with the highest silhouette score and lowest number of clusters'''
+        ss = np.asarray(ss)
+        highest_ss = np.flatnonzero(ss == np.max(ss))                       #indices of highest silhouette scores
+        _cluster = np.take(clusters, highest_ss)                            #number of clusters for highest silhouette score
+        lowest_clusters = np.flatnonzero(clusters == np.min(_cluster))      #indices of lowest clusters 
+        overlaps = [i for i in highest_ss if i in lowest_clusters]
+        return overlaps[0]
+    
 
     def cluster_trajectories(self, approach = 'all', plot = False, table = False):
-
-        Traj, lengths, Traj_plot = [], [], []
+        '''function to create A_star matrix and cluster using sklearn (Affinity Propagation)'''
+        A_star_list, Traj_plot = [], []
         
         for obs in self.obs_list:
             if (approach != 'all' and obs.approach != approach) or obs.f_star == []:
                 continue                    
-            Traj.append(obs.f_star)
-            lengths.append(obs.length)
+            A_star_list.append(obs.f_star)
             Traj_plot.append(obs.trajectory_plot)
             
-        if len(Traj) < 10:
+        if len(A_star_list) < 10:
             return
         
-        Traj_1=np.asarray(Traj)
-        lengths_1 = np.array(lengths)
+        A_star = np.asarray(A_star_list)
         
         X, ss, clusts = [], [], []
-        
         for x in range(-5000,-100,100):
-            
-            af = AffinityPropagation(preference=x, random_state=1).fit(Traj_1)
-
+            af = AffinityPropagation(preference=x, random_state=1).fit(A_star)
             try:
-                silhouette_score = m.silhouette_score(Traj_1, af.labels_)
-                ss.append(silhouette_score)
+                ss.append(m.silhouette_score(A_star, af.labels_))
                 clusts.append(len(af.cluster_centers_indices_))
                 X.append(x)
             except:
@@ -163,9 +160,12 @@ class Clusters(object):
             print('clustering failed - possibly not enough trajectories')
             return 
         
-        self.af = AffinityPropagation(preference=lengths_1+X[np.argmax(ss)], random_state=1).fit(Traj_1)
+        opt = self.find_optimal_preference(ss, clusts)
+        print(np.argmax(ss), opt, X[opt])
+        
+        self.af = AffinityPropagation(preference=X[opt], random_state=1).fit(A_star)
        
-        silhouette_score = m.silhouette_score(Traj_1, af.labels_)
+        silhouette_score = m.silhouette_score(A_star, af.labels_)
         
         if plot == True:
             self.plot_trajectories(Traj_plot, approach)
@@ -175,6 +175,7 @@ class Clusters(object):
       
      
     def output_table(self, approach, silhouette_score):
+        '''creates a text file with information about the clustered pathways'''
         Table = []
         for entry in set(self.af.labels_):
             if np.count_nonzero(self.af.labels_ == entry) < self.cluster_omit:
@@ -191,15 +192,10 @@ class Clusters(object):
                     fp.write(f"{pos},")  
                 fp.write("\n")  
                             
- 
-    def plot_raw_trajectories(self, trajectories, approach):
-        image = cv2.imread(self.filedirectory+'Geometry/plan.png')
-        for traj in trajectories:
-            cvutils.cvPlot(image, traj, (255, 0, 0), traj.length())  
-        cv2.imwrite(self.filedirectory+'clustering/raw_trajectories_trimmed{}.jpg'.format(approach), image)
     
  
     def plot_trajectories(self, Full_traj, approach):
+        '''function to plot all trajectories by cluster and plot the cluster exemplars with additional information'''
         import matplotlib.colors as colors
         
         image_all = cv2.imread(self.filedirectory+'Geometry/plan.png')
@@ -270,6 +266,7 @@ class Intersection(object):
     
     
     def point_input(self, image, info):
+        '''function to get coordinate points from user clicking on image'''
         print(info[0])
         plt.figure(figsize=(5,5))
         plt.imshow(image)
@@ -296,12 +293,12 @@ class Intersection(object):
 
 
     def P_ave(self,p1,p2):
-        #return the midpoint of a line / average of two points
+        '''return the midpoint of a line / average of two points'''
         return SG.Point((p1[0]+p2[0])/2,(p1[1]+p2[1])/2)
 
 
     def plotPoly(self):
-        #plot polygons on world image and save image
+        '''plot polygons on world image and save image'''
         image = cv2.imread(self.filedirectory+'/plan.png')
         out_file = self.filedirectory+'/approach_polys.jpg'
         for app, poly in self.approach_polys.items():
@@ -311,18 +308,10 @@ class Intersection(object):
             cv2.imwrite(out_file, image)
 
 
-    def extend_Line(self,P1,P2,distance):
-        m = (P2[1]- P1[1]) / (P2[0] - P1[0])
-        theta = np.arctan(m)
-        x = np.sign(P2[0] - P1[0])*distance*np.cos(theta) + P1[0]
-        y = np.sign(P2[1] - P1[1])*distance*np.sin(theta) + P1[1]
-        return SG.Point(x,y)
-        
-
     def polys(self, directions = ['N','E','S','W'], save = False):
-        #create polygons from points in the middle of the approach arms and the center of the intersection.
-        #directions is input if it is not a four-approach intersection
-        #save is a file pathways
+        '''create polygons from points in the middle of the approach arms and the center of the intersection.
+        directions is input if it is not a four-approach intersection
+        save is a file pathways'''
         C = self.center[0]
         arm_centers_extend = []
         for row in self.arm_centers:
